04a67c3b1cbbf8aaaf82fa26ce039f5837828ff6
ste <mrbarrow@hotmail.co.uk>
2024-07-16 16:28:15 +0100

    feat: added fields to user

--- src/main/java/com/yanader/foodtruck/model/User.java
+++ src/main/java/com/yanader/foodtruck/model/User.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.time.LocalDate;
+
 @Table(name="users")
 @Entity
 public class User {
     @Id
     private long userId;
     private String userName;
+    private String userEmail;
     private UserType userType;
+    private LocalDate createdOn;
 }
 
--- src/main/java/com/yanader/foodtruck/model/UserType.java
+++ src/main/java/com/yanader/foodtruck/model/UserType.java
@@ -1,5 +1,7 @@
 package com.yanader.foodtruck.model;
 
+import jakarta.persistence.Entity;
+
 public enum UserType {
     TRUCK,
     CUSTOMER
