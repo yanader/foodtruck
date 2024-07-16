d411776e8cf15f5d7a346ee68dfed84b3b415dfd
ste <mrbarrow@hotmail.co.uk>
2024-07-16 19:43:12 +0100

    feat: initialised TruckController

---
+++ src/main/java/com/yanader/foodtruck/controller/TruckController.java
@@ -0,0 +1,23 @@
+package com.yanader.foodtruck.controller;
+
+import com.yanader.foodtruck.model.FoodTruck;
+import com.yanader.foodtruck.service.TruckService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TruckController {
+
+    @Autowired
+    TruckService truckService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<FoodTruck>> getAllFoodTrucks() {
+        Iterable<FoodTruck> allTrucks = truckService.getAllTrucks();
+        return new ResponseEntity<>(allTrucks, HttpStatus.OK);
+    }
+}
